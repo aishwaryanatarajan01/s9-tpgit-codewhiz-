afcaf2a9c493a1946cd40511fbd03a9bfb76f69a
aishwaryanatarajan01 <aishwariyaaishu186@gmail.com>
2026-01-07 16:26:03 +0530

    Create 4.Monthly expense tracker.py

---
+++ 4.Monthly expense tracker.py
@@ -0,0 +1,11 @@
+a=int(input("enter the number of items bought:"))
+y=0
+for i in range(0,a):
+     b=input("enter category of amount you spent:")
+     c=float(input("enter amount you spend"))
+     y+=c
+print("total expenses:",y)
+
+
+
+
